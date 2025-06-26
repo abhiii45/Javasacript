55e9b557ec4e0c8a3fa20e21d3ecda5207e77966
Abhinandan Patil <135232684+abhiii45@users.noreply.github.com>
2025-06-26 17:10:15 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,22 @@
+let score = undefined
+
+//console.log(typeof score);
+
+let value=Number(score)
+// console.log(typeof value);
+// console.log(value);
+
+
+//"40" =>40 
+// "40scs" => Nan
+
+let isLooged="Abhii"
+let booleanIsLoggedIn = Boolean(isLooged)
+console.log(booleanIsLoggedIn );
+
+//1=>true
+//0=>false
+//""=>false
+//"abhii"=>true
+
+
