3873fd7583fd6bea25d45d15e1da78dd05cc0d69
Abhinandan Patil <135232684+abhiii45@users.noreply.github.com>
2025-06-26 16:57:40 +0000

    datatype

---
+++ datatypes/data.js
@@ -0,0 +1,19 @@
+let name="Abhii"//string
+let age=20 //integer
+let isLoggIn=true //boolen
+
+//bigint
+//number =>  to power 
+//string => "" ''
+//boolean =>true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+
+
+//object
+console.log(typeof "abhii"); //object
+
