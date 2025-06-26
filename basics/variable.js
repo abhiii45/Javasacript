5befcea73a5b14d581e4fcdda80169594d39f74e
Abhinandan Patil <135232684+abhiii45@users.noreply.github.com>
2025-06-26 16:43:39 +0000

    variable and its use

---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const accId = 42
+let accmail ="abhii@123.com"
+var accpass ="526341"
+acccity="kop"
+// accId = 2 //not allowed beacause we cant change the const value 
+
+accmail="heh234.COM"
+accpass ="748596"
+acccity="pune"
+
+console.log(accId);
+/* 
+prefer not to use var
+beacause of issue in block scope and functionsl scope
+*/
+console.table([accmail,accpass,acccity])
+
